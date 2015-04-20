9436c75fa51038cd4fcc1c42bfe0820fc409397e
Elizabeth Dutton <emddutton@gmail.com>
2015-04-20 14:33:19 -0600

    under construction

---
+++ gemini.js
@@ -0,0 +1,7 @@
+/**
+ * Created by emddutton on 4/19/2015.
+ */
+$(document).ready(function(){
+   $(".text h3").addClass('animated bounceInDown');
+    $(".social-icons").addClass('animated bounceInDown');
+});
