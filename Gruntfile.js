ba6e60d39745bc37e193d2c4c4d4bf9300c6d3a7
Elizabeth Dutton <emddutton@gmail.com>
2015-12-22 15:03:04 -0700

    getting set up

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,26 +1,43 @@
-module.exports = function(grunt) {
 
-    // 1. All configuration goes here 
+
+    // concat: {   
+    //     dist: {
+    //         src: [
+    //             'js/libs/*.js', // All JS in the libs folder
+    //             'js/global.js'  // This specific file
+    //         ],
+    //         dest: 'js/build/production.js',
+    //     }
+    // }
+
+
+module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-
-    concat: {   
-        dist: {
-            src: [
-                'js/libs/*.js', // All JS in the libs folder
-                'js/global.js'  // This specific file
-            ],
-            dest: 'js/build/production.js',
+        sass: {
+            dist: {
+                options: {
+                    lineNumber: true,
+                    debugInfo: false,
+                    style: 'expanded'
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'css/src',
+                    src: ['*.scss'],
+                    dest: 'css/build',
+                    ext: '.css'
+                }]
+            }
+        },
+        watch: {
+            css: {
+                files: 'css/src/*.scss',
+                tasks: ['sass']
+            }
         }
-    }
-
     });
-
-    // 3. Where we tell Grunt we plan to use this plug-in.
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-
-    // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['concat', 'uglify']);
-
-};
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default',['watch']);
+}
