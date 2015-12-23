c7789f868e8a2a9a18e073ae0c75fe0b3b0705d0
Elizabeth Dutton <emddutton@gmail.com>
2015-12-22 21:07:25 -0700

    grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -42,30 +42,4 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.registerTask('default',['watch']);
 }
-=======
-module.exports = function(grunt) {
 
-    // 1. All configuration goes here 
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-
-    concat: {   
-        dist: {
-            src: [
-                'js/libs/*.js', // All JS in the libs folder
-                'js/global.js'  // This specific file
-            ],
-            dest: 'js/build/production.js',
-        }
-    }
-
-    });
-
-    // 3. Where we tell Grunt we plan to use this plug-in.
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-
-    // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['concat', 'uglify']);
-
-};
